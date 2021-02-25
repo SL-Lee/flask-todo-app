613abe273daf574000518a360148f7746ef1e8a1
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2021-02-25 13:02:42 +0800

    Update index page
    Also:
    
    - Remove sidebar (since there was no use for it)
    - Fix typo in application configurations (`SESSION_COOKIE_SAME_SITE` ->
      `SESSION_COOKIE_SAMESITE`)

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config.update(
     SECRET_KEY=os.urandom(32),
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
     SQLALCHEMY_DATABASE_URI="sqlite:///db.sqlite3",
-    SESSION_COOKIE_SAME_SITE="Lax",
+    SESSION_COOKIE_SAMESITE="Lax",
 )
 
 app.register_blueprint(api_blueprint.api_blueprint)
